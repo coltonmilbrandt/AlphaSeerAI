8b60b892a81a7925b4075c623b8ea2c3815ccd62
coltonmilbrandt <coltonmilbrandt@gmail.com>
2023-03-14 16:15:33 -0600

    fixed leak

--- server/server.js
+++ server/server.js
@@ -6,4 +6,11 @@ dotenv.config({ path: "./config/config.env" })
 
 const app = express()
 
-app.listen()
+const PORT = process.env.PORT || 5000
+
+app.listen(
+	PORT,
+	console.log(
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+	)
+)
