468297b19a5343097baf99822858c268c2bd9464
coltonmilbrandt <coltonmilbrandt@gmail.com>
2023-04-10 17:59:20 -0600

    changed navbar to typescript

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,61 +3,62 @@ import Image from "next/image"
 import { Inter } from "@next/font/google"
 import styles from "../styles/Home.module.css"
 import Link from "next/link"
+import React from "react"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export default function Navbar() {
 	return (
 		<>
-			<nav class="bg-gray-800 z-50">
-				<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-					<div class="flex h-16 items-center justify-between">
-						<div class="flex items-center">
-							<div class="flex-shrink-0">
+			<nav className="bg-gray-800 z-50">
+				<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+					<div className="flex h-16 items-center justify-between">
+						<div className="flex items-center">
+							<div className="flex-shrink-0">
 								<a href="/">
 									<img
-										class="h-8 w-8"
+										className="h-8 w-8"
 										src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
 										alt="Your Company"
 									/>
 								</a>
 							</div>
-							<div class="hidden md:block">
-								<div class="ml-10 flex items-baseline space-x-4">
+							<div className="hidden md:block">
+								<div className="ml-10 flex items-baseline space-x-4">
 									{/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
 									<Link
 										href="/dashboard"
-										class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+										className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
 									>
 										Dashboard
 									</Link>
 									<a
 										href="/signup"
-										class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+										className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
 									>
 										Sign Up
 									</a>
 									<a
 										href="/login"
-										class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+										className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
 									>
 										Login
 									</a>
 								</div>
 							</div>
 						</div>
-						<div class="hidden md:block">
-							<div class="ml-4 flex items-center md:ml-6">
+						<div className="hidden md:block">
+							<div className="ml-4 flex items-center md:ml-6">
 								<button
 									type="button"
-									class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+									className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
 								>
-									<span class="sr-only">
+									<span className="sr-only">
 										View notifications
 									</span>
 									{/* <!-- Heroicon name: outline/bell --> */}
 									<svg
-										class="h-6 w-6"
+										className="h-6 w-6"
 										xmlns="http://www.w3.org/2000/svg"
 										fill="none"
 										viewBox="0 0 24 24"
@@ -74,20 +75,20 @@ export default function Navbar() {
 								</button>
 
 								{/* <!-- Profile dropdown --> */}
-								<div class="relative ml-3">
+								<div className="relative ml-3">
 									<div>
 										<button
 											type="button"
-											class="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+											className="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
 											id="user-menu-button"
 											aria-expanded="false"
 											aria-haspopup="true"
 										>
-											<span class="sr-only">
+											<span className="sr-only">
 												Open user menu
 											</span>
 											<img
-												class="h-8 w-8 rounded-full"
+												className="h-8 w-8 rounded-full"
 												src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
 												alt=""
 											/>
@@ -106,18 +107,18 @@ export default function Navbar() {
               --> */}
 									<div
 										// TEMPORARILY "HIDDEN"
-										class="hidden absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+										className="hidden absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
 										role="menu"
 										aria-orientation="vertical"
 										aria-labelledby="user-menu-button"
-										tabindex="-1"
+										tabIndex={-1}
 									>
 										{/* <!-- Active: "bg-gray-100", Not Active: "" --> */}
 										<a
 											href="#"
-											class="block px-4 py-2 text-sm text-gray-700"
+											className="block px-4 py-2 text-sm text-gray-700"
 											role="menuitem"
-											tabindex="-1"
+											tabIndex={-1}
 											id="user-menu-item-0"
 										>
 											Your Profile
@@ -125,9 +126,9 @@ export default function Navbar() {
 
 										<a
 											href="#"
-											class="block px-4 py-2 text-sm text-gray-700"
+											className="block px-4 py-2 text-sm text-gray-700"
 											role="menuitem"
-											tabindex="-1"
+											tabIndex={-1}
 											id="user-menu-item-1"
 										>
 											Settings
@@ -135,9 +136,9 @@ export default function Navbar() {
 
 										<a
 											href="#"
-											class="block px-4 py-2 text-sm text-gray-700"
+											className="block px-4 py-2 text-sm text-gray-700"
 											role="menuitem"
-											tabindex="-1"
+											tabIndex={-1}
 											id="user-menu-item-2"
 										>
 											Sign out
@@ -146,22 +147,22 @@ export default function Navbar() {
 								</div>
 							</div>
 						</div>
-						<div class="-mr-2 flex md:hidden">
+						<div className="-mr-2 flex md:hidden">
 							{/* <!-- Mobile menu button --> */}
 							<button
 								type="button"
-								class="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+								className="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
 								aria-controls="mobile-menu"
 								aria-expanded="false"
 							>
-								<span class="sr-only">Open main menu</span>
+								<span className="sr-only">Open main menu</span>
 								{/* <!--
               Heroicon name: outline/bars-3
 
               Menu open: "hidden", Menu closed: "block"
             --> */}
 								<svg
-									class="block h-6 w-6"
+									className="block h-6 w-6"
 									xmlns="http://www.w3.org/2000/svg"
 									fill="none"
 									viewBox="0 0 24 24"
@@ -181,7 +182,7 @@ export default function Navbar() {
               Menu open: "block", Menu closed: "hidden"
             --> */}
 								<svg
-									class="hidden h-6 w-6"
+									className="hidden h-6 w-6"
 									xmlns="http://www.w3.org/2000/svg"
 									fill="none"
 									viewBox="0 0 24 24"
@@ -201,12 +202,12 @@ export default function Navbar() {
 				</div>
 
 				{/* <!-- Mobile menu, show/hide based on menu state. --> */}
-				<div class="md:hidden" id="mobile-menu">
-					<div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
+				<div className="md:hidden" id="mobile-menu">
+					<div className="space-y-1 px-2 pt-2 pb-3 sm:px-3">
 						{/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
 						<a
 							href="#"
-							class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
+							className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
 							aria-current="page"
 						>
 							Dashboard
@@ -214,57 +215,57 @@ export default function Navbar() {
 
 						<a
 							href="#"
-							class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+							className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
 						>
 							Team
 						</a>
 
 						<a
 							href="#"
-							class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+							className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
 						>
 							Projects
 						</a>
 
 						<a
 							href="#"
-							class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+							className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
 						>
 							Calendar
 						</a>
 
 						<a
 							href="#"
-							class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+							className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
 						>
 							Reports
 						</a>
 					</div>
-					<div class="border-t border-gray-700 pt-4 pb-3">
-						<div class="flex items-center px-5">
-							<div class="flex-shrink-0">
+					<div className="border-t border-gray-700 pt-4 pb-3">
+						<div className="flex items-center px-5">
+							<div className="flex-shrink-0">
 								<img
-									class="h-10 w-10 rounded-full"
+									className="h-10 w-10 rounded-full"
 									src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
 									alt=""
 								/>
 							</div>
-							<div class="ml-3">
-								<div class="text-base font-medium leading-none text-white">
+							<div className="ml-3">
+								<div className="text-base font-medium leading-none text-white">
 									Tom Cook
 								</div>
-								<div class="text-sm font-medium leading-none text-gray-400">
+								<div className="text-sm font-medium leading-none text-gray-400">
 									tom@example.com
 								</div>
 							</div>
 							<button
 								type="button"
-								class="ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+								className="ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
 							>
-								<span class="sr-only">View notifications</span>
+								<span className="sr-only">View notifications</span>
 								{/* <!-- Heroicon name: outline/bell --> */}
 								<svg
-									class="h-6 w-6"
+									className="h-6 w-6"
 									xmlns="http://www.w3.org/2000/svg"
 									fill="none"
 									viewBox="0 0 24 24"
@@ -280,24 +281,24 @@ export default function Navbar() {
 								</svg>
 							</button>
 						</div>
-						{/* <div class="mt-3 space-y-1 px-2">
+						{/* <div className="mt-3 space-y-1 px-2">
 							<a
 								href="#"
-								class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+								className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
 							>
 								Your Profile
 							</a>
 
 							<a
 								href="#"
-								class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+								className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
 							>
 								Settings
 							</a>
 
 							<a
 								href="#"
-								class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+								className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
 							>
 								Sign out
 							</a>
