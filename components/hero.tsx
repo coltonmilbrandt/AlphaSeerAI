a51f7658b823ede825cc561d5ec0aaf4c2a3d6ac
coltonmilbrandt <coltonmilbrandt@gmail.com>
2023-04-10 18:02:36 -0600

    changed hero.js to typescript

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,29 +3,30 @@ import Image from "next/image"
 import { Inter } from "@next/font/google"
 import styles from "../styles/Home.module.css"
 import Link from "next/link"
+import React from "react"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export default function Hero() {
 	return (
 		<>
-			<div class="isolate bg-white">
-				<div class="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"></div>
+			<div className="isolate bg-white">
+				<div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"></div>
 				<main>
-					<div class="relative px-6 lg:px-8">
-						<div class="mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40">
+					<div className="relative px-6 lg:px-8">
+						<div className="mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40">
 							<div>
-								<div class="hidden sm:mb-8 sm:flex sm:justify-center">
-									<div class="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-										<span class="text-gray-600">
+								<div className="hidden sm:mb-8 sm:flex sm:justify-center">
+									<div className="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+										<span className="text-gray-600">
 											Why those who don't use AI will be
 											left in the dust.{" "}
 											<a
 												href="#"
-												class="font-semibold text-indigo-600"
+												className="font-semibold text-indigo-600"
 											>
 												<span
-													class="absolute inset-0"
+													className="absolute inset-0"
 													aria-hidden="true"
 												></span>
 												Read more{" "}
@@ -37,21 +38,21 @@ export default function Hero() {
 									</div>
 								</div>
 								<div>
-									<h1 class="text-4xl font-bold tracking-tight sm:text-center sm:text-5xl">
+									<h1 className="text-4xl font-bold tracking-tight sm:text-center sm:text-5xl">
 										Get Tomorrow's Alpha Today.
 									</h1>
-									<p class="mt-6 text-lg leading-8 text-gray-600 sm:text-center">
-										See&nbsp;into&nbsp;the&nbsp;future&nbsp;with&nbsp;AI-powered&nbsp;charts,&nbsp;and&nbsp;analyze
+									<p className="mt-6 text-lg leading-8 text-gray-600 sm:text-center">
+										Use&nbsp;AlphaSeer&nbsp;to&nbsp;see&nbsp;into&nbsp;the&nbsp;future&nbsp;with&nbsp;AI-powered&nbsp;charts,&nbsp;and&nbsp;analyze
 										emerging&nbsp;facts&nbsp;and&nbsp;sentiment&nbsp;quicker&nbsp;than&nbsp;ever&nbsp;before.
 									</p>
-									<div class="mt-8 flex gap-x-4 sm:justify-center">
+									<div className="mt-8 flex gap-x-4 sm:justify-center">
 										<a
 											href="#"
-											class="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700"
+											className="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700"
 										>
 											Get started
 											<span
-												class="text-indigo-200"
+												className="text-indigo-200"
 												aria-hidden="true"
 											>
 												&rarr;
@@ -59,15 +60,15 @@ export default function Hero() {
 										</a>
 										<Link
 											href="/dashboard"
-											class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+											className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
 										>
 											Dashboard
 										</Link>
 									</div>
 								</div>
-								<div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
+								<div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
 									<svg
-										class="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
+										className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
 										viewBox="0 0 1155 678"
 										fill="none"
 										xmlns="http://www.w3.org/2000/svg"
