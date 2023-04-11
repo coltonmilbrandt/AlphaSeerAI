d034cde9e0ebcc3f208d01053c798d9c1b4dc095
coltonmilbrandt <coltonmilbrandt@gmail.com>
2023-04-10 18:09:56 -0600

    changing over to typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css"
 import "@fortawesome/fontawesome-svg-core/styles.css"
 import { config } from "@fortawesome/fontawesome-svg-core"
+import React from "react"
 // skip adding the CSS automatically since it's being imported above
 config.autoAddCss = false
 
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -2,6 +2,7 @@ import Head from "next/head"
 import Navbar from "../components/navbar"
 import { useRouter } from "next/router"
 import Chart from "../components/chart"
+import React from "react"
 
 export default function Signup() {
 	const router = useRouter()
@@ -25,65 +26,65 @@ export default function Signup() {
   This example requires updating your template:
 
   ```
-  <html class="h-full bg-gray-100">
-  <body class="h-full">
+  <html className="h-full bg-gray-100">
+  <body className="h-full">
   ```
 --> */}
-				<div class="dark:bg-darkBG min-h-screen">
-					<header class="bg-white dark:bg-darkHeader shadow">
-						<div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-							<h1 class="text-3xl font-bold tracking-tight text-gray-900 dark:text-neutral-200">
+				<div className="dark:bg-darkBG min-h-screen">
+					<header className="bg-white dark:bg-darkHeader shadow">
+						<div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+							<h1 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-neutral-200">
 								Sign Up
 							</h1>
 						</div>
 					</header>
 					<main>
-						<div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+						<div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
 							{/* <!-- Replace with your content --> */}
-							<div class="px-4 py-6 sm:px-0">
-								<div class="h-96 rounded-lg">
-									<div class="w-full max-w-xs">
-										<form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-											<div class="mb-4">
+							<div className="px-4 py-6 sm:px-0">
+								<div className="h-96 rounded-lg">
+									<div className="w-full max-w-xs">
+										<form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+											<div className="mb-4">
 												<label
-													class="block text-gray-700 text-sm font-bold mb-2"
-													for="username"
+													className="block text-gray-700 text-sm font-bold mb-2"
+													htmlFor="username"
 												>
 													Username
 												</label>
 												<input
-													class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+													className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
 													id="username"
 													type="text"
 													placeholder="Username"
 												/>
 											</div>
-											<div class="mb-6">
+											<div className="mb-6">
 												<label
-													class="block text-gray-700 text-sm font-bold mb-2"
-													for="password"
+													className="block text-gray-700 text-sm font-bold mb-2"
+													htmlFor="password"
 												>
 													Password
 												</label>
 												<input
-													class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+													className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
 													id="password"
 													type="password"
 													placeholder="******************"
 												/>
-												<p class="text-red-500 text-xs italic">
+												<p className="text-red-500 text-xs italic">
 													Please choose a password.
 												</p>
 											</div>
-											<div class="flex items-center justify-between">
+											<div className="flex items-center justify-between">
 												<button
-													class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+													className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
 													type="button"
 												>
 													Sign In
 												</button>
 												<a
-													class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+													className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
 													href="#"
 												>
 													Forgot Password?
